8b5847ce4fe039a5bfaaf948008a08197fa4fa33
mahuanfy <mahuanfy@foxmail.com>
2018-04-24 10:00:58 +0800

    完成其余四个接口测试

--- Test/test_oj_subject.py
+++ Test/test_oj_subject.py
@@ -36,6 +36,15 @@ def wrong_question(uuid):
     url = "%s/wrongTitle/selectStudentWrongTitlteNo?uuid=%s" % (get_baseurl(), uuid)
     return get_response(url)
 
+# 删除试题通过试题id和学生id
+def delete_wrong(subject_id,stuId):
+    url = "%s/wrongTitle/deleteWrongTitle?id=%s&stu=%s" % (get_baseurl(),subject_id,stuId)
+    return get_response(url)
+
+def select_score(stuId,classId):
+    url = "%s/studentTestpaper/queryScore?stuId=%s&&classId=%s" % (get_baseurl(),stuId,classId)
+    return get_response(url)
+
 def test_oper_subject():
     data = select_subject()
     if None == data:
@@ -66,8 +75,40 @@ def test_wrong_question(uuid):
     return True
 
 
+def test_delete_wrong_question(subject_id,stuId):
+    data = delete_wrong(subject_id,stuId)
+
+    if data['result'] != True:
+        print '删除试题失败'
+        return False
+
+    if data['data'] != None:
+        print '删除试题失败'
+        return False
+
+    return True
+
+def test_select_score(stuId,classId):
+    data = select_score(stuId,classId)
+
+    if data['stateCode'] != '200':
+        print '成绩查询失败'
+        return False
+
+    if data['data'][0]['testpaper_student_score'] != 100.0:
+        print '成绩查询失败'
+        return False
+
+    return True
+
 ret = test_oper_subject()
 ASSERT_TRUE(ret, '查询试题库中的所有试题！')
 
 ret = test_wrong_question('zNh2AYiU6Sm09POw')
 ASSERT_TRUE(ret, '查询所有错题！')
+
+ret = test_delete_wrong_question(2,3)
+ASSERT_TRUE(ret, '删除试题！')
+
+ret = test_select_score(3,4)
+ASSERT_TRUE(ret, '成绩查询!')
