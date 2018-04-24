87cf9ead3b66fa7ced23c228d02fa79bb3d68638
PersistXL <pointinglxl@gmail.com>
2018-04-24 16:51:21 +0800

    '增加通过classId查询试卷的测试脚本'

---
+++ Test/test_oj_studentTestpaper.py
@@ -0,0 +1,47 @@
+import urllib2
+import json
+import random
+import string
+
+def ASSERT_NONE(a, name):
+    if a == None:
+        print '[OK] ' + name;
+    else:
+        print '[BAD] ' + name;
+
+def ASSERT_TRUE(a, name):
+    if a:
+        print '[OK] ' + name;
+    else:
+        print '[BAD] ' + name;
+
+
+def get_baseurl():
+    return 'http://10.53.0.34:8080/oj'
+
+
+def get_response(url):
+    try:
+        jstr = urllib2.urlopen(url).read().decode(encoding='UTF-8',errors='strict')
+        return json.loads(jstr)
+    except:
+        return None
+
+def query_stuTestpaper(classesId):
+    url = "%s/studentTestpaper/studentTestpaperTitle?classesId=%s" % (get_baseurl(), classesId)
+    return get_response(url)
+
+def test_student_testpaper(classesId):
+    data = query_stuTestpaper(classesId)
+
+    if data['result'] != True:
+        print 'query failed'
+        return False
+    if data['data'][0]['score'] != 100.0:
+        print 'QUERY FAILED'
+        return False
+    return True
+
+
+ret = test_student_testpaper(1)
+ASSERT_TRUE(ret, 'select from exam_test')
