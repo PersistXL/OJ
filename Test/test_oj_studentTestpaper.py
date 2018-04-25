6a0d7796e8a9a3dbd4878c35989f1f2f959bc4b5
PersistXL <pointinglxl@gmail.com>
2018-04-25 20:52:49 +0800

    增加存储学生的错题信息及成绩

--- Test/test_oj_studentTestpaper.py
+++ Test/test_oj_studentTestpaper.py
@@ -19,7 +19,6 @@ def ASSERT_TRUE(a, name):
 def get_baseurl():
     return 'http://10.53.0.34:8080/oj'
 
-
 def get_response(url):
     try:
         jstr = urllib2.urlopen(url).read().decode(encoding='UTF-8',errors='strict')
@@ -29,6 +28,11 @@ def get_response(url):
 
 def query_stuTestpaper(classesId):
     url = "%s/studentTestpaper/studentTestpaperTitle?classesId=%s" % (get_baseurl(), classesId)
+    return get_response(url)
+
+
+def add_studentScore(testpaperjson):
+    url = "%s/studentTestpaper/commitScore?testpaperjson=%s" % (get_baseurl(),testpaperjson)
     return get_response(url)
 
 def test_student_testpaper(classesId):
@@ -42,6 +46,16 @@ def test_student_testpaper(classesId):
         return False
     return True
 
+def test_add_studentScore(testpaperjson):
+    data = add_studentScore(testpaperjson)
+    if data['result'] != True:
+        print 'inseret failed'
+        return False
 
+    return True
+    
 ret = test_student_testpaper(1)
 ASSERT_TRUE(ret, 'select from exam_test')
+
+ret = test_add_studentScore("%7B'studentId':'2166','testpaperId':'22','testpaperStudentScore':99.5,'data':[%7B'id':'40','option':'D'%7D]%7D")
+ASSERT_TRUE(ret,'Store student stest scores and errors')
